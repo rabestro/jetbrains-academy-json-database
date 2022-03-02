73b948398a3ba3f37b9d8928fbf78a9036bbb8ea
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2022-03-02 19:01:11 +0200

    Solved stage 1

--- JSON Database/task/src/server/Broker.java
+++ JSON Database/task/src/server/Broker.java
@@ -6,7 +6,7 @@ public record Broker(Repository repository) implements UnaryOperator<String> {
 
     @Override
     public String apply(final String request) {
-        var args = request.split("\\s+");
+        var args = request.split("\\s+", 3);
         var key = Integer.parseInt(args[1]);
         return switch (args[0]) {
             case "get" -> repository().get(key);
